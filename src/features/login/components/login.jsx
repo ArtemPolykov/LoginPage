f931ce0b3a83f02812454c7f9c55e25a4d61fb7b
a_polyakov <a_polyakov@head-point.ru>
2023-01-08 16:05:03 +0300

    fix background

--- src/features/login/components/login.jsx
+++ src/features/login/components/login.jsx
@@ -13,28 +13,28 @@ export default function Login(props) {
     }
 
     return (
-        <div className='background'>
-            <form className='login-form'>
-                <img className='login-form__logo' src='img/auth_logo.png' alt='img'/>
-                <div className='login-form__input'>
-                    <h2 className='login-form__header'>Your account:</h2>
-                    <div className='input-flex'>
-                        <img className='input-flex__img' src='img/mail.png'  alt='img'/>
-                        <input className='input-flex__input' type='text' placeholder='Email' value={login} onChange={e => setLogin(e.value)} />
-                    </div>
-                    <div className='input-flex'>
-                        <img className='input-flex__img' src='img/open-lock.png'  alt='img'/>
-                        <input className='input-flex__input' type={showPass ? 'text' : 'password'} placeholder='Password' value={pass} onChange={e => setPass(e.value)} />
-                        <button className='input-flex__button' type='button' onClick={e => setShowPass(!showPass)}>
-                            <img className='input-flex__img' src='img/eye.png'  alt='img'/>
-                        </button>
-                    </div>
-                    <button className='login-form__submit' type='button' onClick={LogIn}>LogIn</button>
+        <form className='login-form'>
+            <div className='login-form__logo-wrapper'>
+                <img className='login-form__logo-image' src='img/auth_logo.png' alt='img' />
+            </div>
+            <div className='login-form__input'>
+                <h2 className='login-form__header'>Your account:</h2>
+                <div className='input-flex'>
+                    <img className='input-flex__img' src='img/mail.png' alt='img' />
+                    <input className='input-flex__input' type='text' placeholder='Email' value={login} onChange={e => setLogin(e.value)} />
                 </div>
-                <div className='login-form__signin'>
-                    <a href='/'>Create account</a>
+                <div className='input-flex'>
+                    <img className='input-flex__img' src='img/open-lock.png' alt='img' />
+                    <input className='input-flex__input' type={showPass ? 'text' : 'password'} placeholder='Password' value={pass} onChange={e => setPass(e.value)} />
+                    <button className='input-flex__button' type='button' onClick={e => setShowPass(!showPass)}>
+                        <img className='input-flex__img' src='img/eye.png' alt='img' />
+                    </button>
                 </div>
-            </form>
-        </div>
+                <button className='login-form__submit' type='button' onClick={LogIn}>LogIn</button>
+            </div>
+            <div className='login-form__signin'>
+                <a href='/'>Create account</a>
+            </div>
+        </form>
     );
 }
